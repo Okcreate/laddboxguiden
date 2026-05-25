d3e4b725358324655a85ce9bf4c7dc33b5293887
Oscar Hjert <oscar.cl@hotmail.com>
2026-05-25 10:49:33 +0200

    Fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,19 +1,21 @@
-export default function sitemap() {
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://laddboxguiden.vercel.app",
+      url: 'https://laddboxguiden.vercel.app',
       lastModified: new Date(),
     },
     {
-      url: "https://laddboxguiden.vercel.app/city/stockholm",
+      url: 'https://laddboxguiden.vercel.app/city/stockholm',
       lastModified: new Date(),
     },
     {
-      url: "https://laddboxguiden.vercel.app/city/malmo",
+      url: 'https://laddboxguiden.vercel.app/city/malmo',
       lastModified: new Date(),
     },
     {
-      url: "https://laddboxguiden.vercel.app/city/goteborg",
+      url: 'https://laddboxguiden.vercel.app/city/goteborg',
       lastModified: new Date(),
     },
   ]
