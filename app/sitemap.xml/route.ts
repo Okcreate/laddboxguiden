d343f7fd777b29d91530c97f85c0621397afe91b
Oscar Hjert <oscar.cl@hotmail.com>
2026-05-25 11:08:51 +0200

    Clean sitemap setup

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,23 +1,15 @@
 export async function GET() {
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>https://laddboxguiden.vercel.app</loc>
-    </url>
-    <url>
-      <loc>https://laddboxguiden.vercel.app/city/stockholm</loc>
-    </url>
-    <url>
-      <loc>https://laddboxguiden.vercel.app/city/malmo</loc>
-    </url>
-    <url>
-      <loc>https://laddboxguiden.vercel.app/city/goteborg</loc>
-    </url>
-  </urlset>`
-
-  return new Response(xml, {
-    headers: {
-      'Content-Type': 'application/xml',
-    },
-  })
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>https://laddboxguiden.vercel.app</loc>
+      </url>
+    </urlset>`,
+    {
+      headers: {
+        "Content-Type": "application/xml",
+      },
+    }
+  )
 }
